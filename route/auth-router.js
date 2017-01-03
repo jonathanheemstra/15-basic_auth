40147bbf38877fa4b6c4cd3778d7594c222d04aa
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-01-03 09:19:55 -0800

    create post/get routes

---
+++ route/auth-router.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const debug = require('debug')('fomogram:auth-router');
+const jsonParser = require('body-parser').json();
+const Router = require('express').Router;
+const basicAuth = require('../lib/auth-middleware.js');
+const User = require('../model/user.js');
+const authRouter = module.exports = Router();
+
+authRouter.post('/api/signup', jsonParser, function(req, res, next) {
+  debug('POST: /api/signup');
+
+  let password = req.body.password;
+  delete req.body.password;
+
+  let user = new User(req.body);
+
+  user.genPasswordHash(password)
+    .then( user => user.save())
+    .then( user => user.genToken())
+    .then( token => res.send(token))
+    .catch(next);
+});
+
+authRouter.get('/api/signin', basicAuth, function(req, res, next) {
+  debug('GET: /api/signin');
+
+  User.findOne({ username: req.auth.username })
+    .then( user => user.comparePasswordHash(req.auth.password))
+    .then( user => user.genToken())
+    .then( token => res.send(token))
+    .catch(next);
+});
