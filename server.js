1beb7fd18c3528f61032e5ff7a33ee88ace6764f
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-01-02 20:57:14 -0800

    create error middleware

--- server.js
+++ server.js
@@ -2,10 +2,20 @@
 
 const express = require('express');
 const debug = require('debug')('fomogram:server');
-
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGODB_LOCAL_URI;
 const PORT = process.env.PORT;
-
 const app = express();
+
+dotenv.load();
+
+// mongoose.connect(MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
 
 app.listen(PORT, () => {
   debug(`server up: ${PORT}`);
