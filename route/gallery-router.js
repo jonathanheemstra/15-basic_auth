bdcf1c36c4b1b017b9bf6206493e6ea2f1acfa5c
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-01-03 18:13:32 -0800

    set up post routes

---
+++ route/gallery-router.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const createError =require('http-errors');
+const debug = require('debug')('fomogram:gallery-router');
+
+const Gallery = require('../model/gallery.js');
+const bearer = require('../lib/bearer-middleware.js');
+
+const galleryRouter = module.exports = Router();
+
+galleryRouter.post('/api/gallery', bearer, jsonParser, function(req, res, next) {
+  debug('POST: /api/gallery');
+
+  req.body.userID = req.user._id;
+  new Gallery(req.body).save()
+    .then( gallery => res.json(gallery))
+    .catch(next);
+});
+
+galleryRouter.get('/api/gallery/:id', function(req, res, next) {
+  debug('GET: /api/gallery/:id');
+});
+
+galleryRouter.put('/api/gallery/:id', function(req, res, next) {
+  debug('PUT: /api/gallery/:id');
+});
+
+galleryRouter.delete('/api/gallery/:id', function(req, res, next) {
+  debug('DELETE: /api/gallery/:id');
+});
